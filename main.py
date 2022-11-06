7b6e72a9cff523216befcd00745e6890c2848f5a
user <maksim.glinka@gmail.com>
2022-11-06 18:41:25 +0300

    update files

--- main.py
+++ main.py
@@ -2,40 +2,48 @@ import re
 from pprint import pprint
 import csv
 
-with open('phonebook_raw.csv') as f:
-    rows = csv.reader(f, delimiter=',')
-    contact_list = list(rows)
-pprint(contact_list)
+phone_pattern = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
+text_pattern = r'+7(\2)-\3-\4-\5 \6\7'
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
 
-phone_list = re.complier(r'(\+7|8)\s*\(?(\d{3})\)?(\s*|-)(\d{3})(\s*|-*)(\d{2})-?(\d{2})(\s*(\(?(\доб.)?)\s*(\d{4}))?(\))*')
-text_list = re.complier(r'(\w+[А-яЁё])\s*\,*(\w+[А-яЁё])\s*\,*(\w+[А-яЁё])*\,*(\w+[А-яЁё])*\,*(\w+[А-яЁё]\w+[А-яЁё –]*\–*\s*)*\,*(\+*\d\s*\(*\d+\)*\-*\s*\d+\-*\d+\-*\d+\s*\(*\w*\.*\s*\d*\)*)*\,*(\w+\.*\w*\@\w+\.\w+)*')
+
+def main(contact_list: list):
+    new_list = list()
+    for item in contact_list:
+        full_name = ' '.join(item[:3]).split(' ')
+        result = [full_name[0], full_name[1], full_name[2], item[3], item[4],
+                  re.sub(phone_pattern, text_pattern, item[5]),
+                  item[6]]
+        new_list.append(result)
+    return union(new_list)
 
 
-new_contact_list = []
-for i in range(len(contact_list)):
-    if i == 0:
-        new_contact_list.append(contact_list[i])
-    else:
-        line = ','.join(contact_list[i])
-        result = re.search(text_pattern, line)
-        new_contacts_list.append(list(result.groups()))
-        if new_contacts_list[i][0] in new_contacts_list:
-            print(new_contacts_list[i][0:3])
-        if new_contact_list[i][5] != None:
-            new_contacts_list[i][5] = phone_pattern.sub(r'+7(\2)\4-\6-\7 \10\11', new_contacts_list[i][5])
+def union(contacts: list):
+    for contact in contacts:
+        first_name = contact[0]
+        last_name = contact[1]
+        for new_contact in contacts:
+            new_first_name = new_contact[0]
+            new_last_name = new_contact[1]
+            if first_name == new_first_name and last_name == new_last_name:
+                if contact[2] == "": contact[2] = new_contact[2]
+                if contact[3] == "": contact[3] = new_contact[3]
+                if contact[4] == "": contact[4] = new_contact[4]
+                if contact[5] == "": contact[5] = new_contact[5]
+                if contact[6] == "": contact[6] = new_contact[6]
+
+    result_list = list()
+    for i in contacts:
+        if i not in result_list:
+            result_list.append(i)
+
+    return result_list
 
 
-final_contact_list = []
-for i in range(len(new_contact_list)):
-    for a in range(len(new_contact_list)):
-        if new_contact_list[i][0] == new_contact_list[a][0]:
-            new_contact_list[i] = [x or y for x, y in zip(new_contact_list[i], new_contact_list[a])]
-            if new_contact_list[i] not in final_contact_list:
-                final_contact_list.append(new_contact_list[i])
-pprint(final_contact_list)
-
-
-with open("phonebook.csv", "w") as f:
+with open("phonebook.csv", "w", encoding="utf-8") as f:
     datawriter = csv.writer(f, delimiter=',')
-    datawriter.writerows(final_contacts_list)
+    datawriter.writerows(main(contacts_list))
